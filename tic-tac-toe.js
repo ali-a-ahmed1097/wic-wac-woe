4dd77152b23fd31805a30d9f23f78d2557885266
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-12 21:08:13 -0500

    Can now click on a tile and set it to a piece.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,10 @@
 // Module
 const Gameboard = (() => {
     // Private
-    let gameboard = [0, 0, 0,
-                     0, 0, 0,
-                     0, 0, 0];
+    let gameboard =
+           [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0];
 
     // Public
     const setGameBoard = (i, piece) => gameboard[i] = piece;
@@ -18,7 +19,7 @@ const Gameboard = (() => {
         else if (gameboard[0] === gameboard[4] === gameboard[8]) return gameboard[0];
         else if (gameboard[2] === gameboard[4] === gameboard[6]) return gameboard[2];
         else if (!(0 in gameboard)) return 0; // Draw
-        
+
         return -1; // No result
     };
 
@@ -45,10 +46,16 @@ const Player = (pName, piece) => {
 const displayController = (() => {
     // Private
     let players = [];
-    
+    let currentPlayer = 0;
+
     // Public
-    const createPlayer = (pName, piece) => { players.push(Player(pName, piece)) };
-    const playMove = (player, tile) => { Gameboard.setGameBoard(tile, players[player].getPiece) };
+    const createPlayer = (pName, piece) => { players.push(Player(pName, piece)); };
+    const playMove = (tile) => {
+        let pc = players[currentPlayer].getPiece();
+        Gameboard.setGameBoard(tile, pc);
+        currentPlayer = currentPlayer ? 0 : 1;
+        return pc;
+    };
 
     return {
         createPlayer,
@@ -58,8 +65,9 @@ const displayController = (() => {
 
 const startBtn = document.getElementById('start');
 const startDiv = document.getElementById('starting');
-const Board = document.getElementById('board');
-//startBtn.classList.toggle('hide');
+const board = document.getElementById('board');
+const tiles = board.querySelectorAll('div');
+
 startBtn.addEventListener('click', function () {
     const p1Name = document.getElementById('p1');
     const p2Name = document.getElementById('p2');
@@ -68,6 +76,21 @@ startBtn.addEventListener('click', function () {
     else {
         startDiv.classList.toggle('hide');
         startDiv.classList.toggle('starting');
-        document.getElementById('board').classList.toggle('hide');
+        board.classList.toggle('hide');
+        displayController.createPlayer(p1Name.value, 1);
+        displayController.createPlayer(p2Name.value, 2);
     }
+});
+
+tiles.forEach((tile) => {
+    tile.addEventListener('click', (e) => {
+        if (tile.textContent === '') {
+            let tn = e.target.id;
+            if (displayController.playMove(Number(tn)) === 1) {
+                tile.textContent = 'X'
+            } else {
+                tile.textContent = 'O';
+            }
+        }
+    });
 });
