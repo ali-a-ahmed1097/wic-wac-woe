3cb930396b209607bdaeb1f4adc71a2983d7168d
Ali <ali-a-ahmed1097@hotmail.com>
2022-12-14 09:46:25 -0500

    Fixed win checking function. Added a display for which player piece would be played on tile hover.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,16 +10,25 @@ const Gameboard = (() => {
     const setGameBoard = (i, piece) => gameboard[i] = piece;
     const getGameBoard = () => gameboard;
     const getWinner = function () {
-        if (gameboard[0] === gameboard[1] === gameboard[2]) return gameboard[0];
-        else if (gameboard[3] === gameboard[4] === gameboard[5]) return gameboard[3];
-        else if (gameboard[6] === gameboard[7] === gameboard[8]) return gameboard[6];
-        else if (gameboard[0] === gameboard[3] === gameboard[6]) return gameboard[0];
-        else if (gameboard[1] === gameboard[4] === gameboard[7]) return gameboard[1];
-        else if (gameboard[2] === gameboard[5] === gameboard[8]) return gameboard[2];
-        else if (gameboard[0] === gameboard[4] === gameboard[8]) return gameboard[0];
-        else if (gameboard[2] === gameboard[4] === gameboard[6]) return gameboard[2];
-        else if (!(0 in gameboard)) return 0; // Draw
+        if (gameboard[0] !== 0) {
+            if (gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2]) return gameboard[0];
+            else if (gameboard[0] === gameboard[3] && gameboard[3] === gameboard[6]) return gameboard[0];
+            else if (gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8]) return gameboard[0];
+        }
 
+        if (gameboard[4] !== 0) {
+            if (gameboard[3] === gameboard[4] && gameboard[4] === gameboard[5]) return gameboard[3];
+            else if (gameboard[1] === gameboard[4] && gameboard[4] === gameboard[7]) return gameboard[1];
+            else if (gameboard[2] === gameboard[4] && gameboard[4] === gameboard[6]) return gameboard[2];
+        }
+        
+        if (gameboard[8] !== 0){
+            if (gameboard[6] === gameboard[7] && gameboard[7] === gameboard[8]) return gameboard[6];
+            else if (gameboard[2] === gameboard[5] && gameboard[5] === gameboard[8]) return gameboard[2];
+        }
+        
+        if (!(gameboard.includes(0))) return 0; // Draw
+        
         return -1; // No result
     };
 
@@ -49,17 +58,25 @@ const displayController = (() => {
     let currentPlayer = 0;
 
     // Public
-    const createPlayer = (pName, piece) => { players.push(Player(pName, piece)); };
+    const createPlayer = (pName, piece) => players.push(Player(pName, piece));
     const playMove = (tile) => {
         let pc = players[currentPlayer].getPiece();
         Gameboard.setGameBoard(tile, pc);
         currentPlayer = currentPlayer ? 0 : 1;
-        return pc;
+    };
+    const getCurrentPiece = () => players[currentPlayer].getPiece();
+    const getWinner = () => {
+        let winner = Gameboard.getWinner();
+        if (winner === -1) return '';
+        else if (winner === 0) return null;
+        return players[winner - 1].getName();
     };
 
     return {
         createPlayer,
-        playMove
+        playMove,
+        getCurrentPiece,
+        getWinner
     };
 })();
 
@@ -67,6 +84,27 @@ const startBtn = document.getElementById('start');
 const startDiv = document.getElementById('starting');
 const board = document.getElementById('board');
 const tiles = board.querySelectorAll('div');
+
+function game(e) {
+    let tile = e.target;
+    if (tile.textContent === '') {
+        if (displayController.getCurrentPiece() === 1) {
+            tile.textContent = 'X';
+        } else {
+            tile.textContent = 'O';
+        }
+        
+        
+    }
+
+    if (e.type === 'click' && !(tile.classList.contains('claimed'))) {
+        tile.classList.add('claimed');
+        displayController.playMove(Number(tile.id));
+        let winner = displayController.getWinner();
+        if (!(winner === '')) {console.log(winner);}
+    }
+    
+}
 
 startBtn.addEventListener('click', function () {
     const p1Name = document.getElementById('p1');
@@ -83,14 +121,7 @@ startBtn.addEventListener('click', function () {
 });
 
 tiles.forEach((tile) => {
-    tile.addEventListener('click', (e) => {
-        if (tile.textContent === '') {
-            let tn = e.target.id;
-            if (displayController.playMove(Number(tn)) === 1) {
-                tile.textContent = 'X'
-            } else {
-                tile.textContent = 'O';
-            }
-        }
-    });
+    tile.addEventListener('mouseover', game);
+    tile.addEventListener('mouseleave', () => { if (!(tile.classList.contains('claimed'))) tile.textContent = ''; });
+    tile.addEventListener('click', game);
 });
